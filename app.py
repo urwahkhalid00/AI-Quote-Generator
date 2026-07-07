769b01bafd66ee3b727ba2dda64768c94c33eae2
Urwah Khalid <urwahkhalid00@gmail.com>
2026-07-07 11:59:50 +0500

    update commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from database import create_database, save_favorite, get_favorites
 import requests
 
 app = Flask(__name__)
@@ -39,5 +40,34 @@ def home():
         author=author,
         selected_category=selected_category
     )
+   
+@app.route("/favorite", methods=["POST"])
+def favorite():
+
+    quote = request.form["quote"]
+
+    author = request.form["author"]
+
+    category = request.form["category"]
+
+    save_favorite(quote, author, category)
+
+    return redirect(url_for("home"))
+
+@app.route("/favorites")
+def favorites():
+
+    quotes = get_favorites()
+
+    return render_template(
+        "favorites.html",
+        quotes=quotes
+    )
+    
+    
+    
 if __name__ == "__main__":
+
+    create_database()
+
     app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+
+def create_database():
+
+    conn = sqlite3.connect("quotes.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS favorites(
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            quote TEXT NOT NULL,
+
+            author TEXT NOT NULL,
+
+            category TEXT NOT NULL
+
+        )
+    """)
+
+    conn.commit()
+
+    conn.close()
+    
+def save_favorite(quote, author, category):
+
+    conn = sqlite3.connect("quotes.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO favorites (quote, author, category)
+        VALUES (?, ?, ?)
+    """, (quote, author, category))
+
+    conn.commit()
+
+    conn.close()    
+    
+def get_favorites():
+
+    conn = sqlite3.connect("quotes.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT * FROM favorites
+    """)
+
+    favorites = cursor.fetchall()
+
+    conn.close()
+
+    return favorites
+
+    
