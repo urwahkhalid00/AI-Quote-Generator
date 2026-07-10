79200820c4c5094e34e3499899193cb36e4d746d
Urwah Khalid <urwahkhalid00@gmail.com>
2026-07-10 11:27:56 +0500

    update commit

--- static/script.js
+++ static/script.js
@@ -1,4 +1,93 @@
 // ===============================
+// Elements
+// ===============================
+
+const themeBtn = document.getElementById("theme-btn");
+
+const body = document.body;
+
+const quote = document.querySelector(".quote-box h2");
+
+const copyBtn = document.querySelectorAll(".action-btn")[0];
+
+// ===============================
+// Dark / Light Mode
+// ===============================
+
+themeBtn.addEventListener("click", () => {
+
+    body.classList.toggle("light-mode");
+
+    if(body.classList.contains("light-mode")){
+
+        themeBtn.innerHTML = "☀️";
+
+        localStorage.setItem("theme","light");
+
+    }
+
+    else{
+
+        themeBtn.innerHTML = "🌙";
+
+        localStorage.setItem("theme","dark");
+
+    }
+
+});
+
+// ===============================
+// Load Saved Theme
+// ===============================
+
+window.onload = () => {
+
+    const savedTheme = localStorage.getItem("theme");
+
+    if(savedTheme === "light"){
+
+        body.classList.add("light-mode");
+
+        themeBtn.innerHTML = "☀️";
+
+    }
+
+}
+
+// ===============================
+// Copy Quote
+// ===============================
+
+const toast = document.getElementById("toast");
+
+copyBtn.addEventListener("click", () => {
+
+    navigator.clipboard.writeText(quote.innerText);
+
+    toast.classList.add("show");
+
+    setTimeout(() => {
+
+        toast.classList.remove("show");
+
+    },2000);
+
+});
+// ===============================
+// Loading Button
+// ===============================
+
+const form = document.querySelector("form");
+
+const generateBtn = document.querySelector(".generate-btn");
+
+form.addEventListener("submit", () => {
+
+    generateBtn.classList.add("loading");
+
+});
+
+// ===============================
 // Favorite Quote
 // ===============================
 
